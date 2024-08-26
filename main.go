d6adb23c51999fcfabf1cde026b78961c6e7c7c8
coskar <oskar.cokl@iprom.si>
2024-08-26 09:30:45 +0200

    feat: create handler now only accepts post requests.

--- main.go
+++ main.go
@@ -20,6 +20,14 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a news snippet..."))
 }
 
