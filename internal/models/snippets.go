16caa88a38f37a823b95c6071c754288499836c4
oskarcokl <oskar.cokl@gmail.com>
2024-08-27 21:48:36 +0200

    feat: Add Latest function to snippets.

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -38,7 +38,7 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 
 
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
-	stmt := `SELECT id, title, content, created, expires from SNIPPETS
+	stmt := `SELECT id, title, content, created, expires FROM snippets
 	WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
 	row := m.DB.QueryRow(stmt, id)
@@ -58,5 +58,32 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 }
 
 func (m* SnippetModel) Latest() ([]*Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	snippets := []*Snippet{}
+
+	for rows.Next() {
+		s := &Snippet{}
+
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
