08498318d910e2d7da189915465a7a2ca3e4c11c
oskarcokl <oskar.cokl@gmail.com>
2024-09-07 21:57:25 +0200

    feat: Now serving with TLS.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -54,6 +54,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		errorLog: errorLog,
@@ -71,7 +72,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
